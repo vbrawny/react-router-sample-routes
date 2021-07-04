4903d77eb47f0af24560d3e580de776a41043732
venky <vbrawny@users.noreply.github.com>
2021-07-04 07:39:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import { BrowserRouter as Router } from "react-router-dom";
+import "./styles.css";
+
+export default function App() {
+  return <Router>{/* All routes are nested inside it */}</Router>;
+}
